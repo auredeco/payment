3456cee9fb32c2b3e6b6a92abcdba3d0b6fdfe7d
Aurelio Decock <aurelio.decock@dx-solutions.be>
2020-02-11 21:06:49 +0100

    Add IndexController and view

---
+++ code/src/Controller/BaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
+    /*
+     * Default helper functions come here.
+     */
+}
---
+++ code/src/Controller/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+class IndexController extends BaseController
+{
+    public function index()
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+}
